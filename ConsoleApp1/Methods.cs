c725ae3e1b0918b8f81219b919fbf653fd58ea0f
Semih İpek <semihipek247@gmail.com>
2023-07-25 16:56:11 +0300

    First Commit

---
+++ ConsoleApp1/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public string position { get; set; }
+    }
+
+    public class BasketballPlayer : Player
+    {
+        public string team { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Player newPlayer1 = new Player
+            {
+                name = "Ümit",
+                position = "Striker",
+
+
+            };
+            BasketballPlayer newPlayer = new BasketballPlayer
+            {
+            name = "Michael Jordan",
+            position = "Shooting Guard",
+            team = "Chicago Bulls"
+        };
+
+            
+
+            Console.WriteLine($"\nPlayer Name :{newPlayer1.name}\nPosition:{newPlayer1.position}\n");
+
+            Console.WriteLine($"Basketball Player Name   :{newPlayer.name}\nPosition:{newPlayer.position}\nTeam:{newPlayer.team} ");
+        } 
+    }
+}
